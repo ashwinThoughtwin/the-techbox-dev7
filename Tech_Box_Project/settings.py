f06cdf0662b4236f08458ef96d84db66aadb1cae
vaibhav881 <mvaibhav002@gmail.com>
2021-05-05 21:26:13 +0530

    resolve static_root issue

--- Tech_Box_Project/settings.py
+++ Tech_Box_Project/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 #media
